80f522107299062dd64edd17231c15826965840b
Marc Granier <mgranier@comcast.net>
2024-04-03 20:32:36 -0400

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,38 @@
+'use client';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin, {
+	Draggable,
+	DropArg,
+} from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+export default function Home() {
+	return (
+		<>
+			<nav className='flex justify-between mb-12 border-b border-violet-100 p-4`'>
+				<h1 className='font-bold text-2xl text-gray-700'> Calendar</h1>
+			</nav>
+			<main className='flex min-h-screen flex-col items-center justify-between p=24'>
+				<div className='grid grid-cols-10'>
+					<div className='col-span-8'>
+						<FullCalendar
+							plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+							headerToolbar={{
+								left: 'prev,next today',
+								center: 'title',
+								right: 'resourceTimelineWook, dayGridMonth,timeGridWeek',
+							}}
+							events={{}}
+							nowIndicator={true}
+							editable={true}
+							droppable={true}
+							selectable={true}
+							selectMirror={true}
+						/>
+					</div>
+				</div>
+			</main>
+		</>
+	);
+}
